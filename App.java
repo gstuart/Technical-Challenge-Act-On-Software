a300f03e0d5dcd2dc2bccebf06441c98dba8f06c
Grace Stuart <gracerstuart@gmail.com>
2017-06-11 14:12:06 -0700

    add method to determine if string is an empty string or not

--- App.java
+++ App.java
@@ -1,6 +1,19 @@
 import java.io.Console;
+import java.util.HashSet;
+import java.util.Set;
 
 public class App {
+  // Back-end logic
+  public static Set<String> permutationOfString(String inputtedString) {
+		Set<String> permutatedInput = new HashSet<String>();
+		if (inputtedString == null) {
+			return null;
+		} else if (inputtedString.length() == 0) {
+			permutatedInput.add("");
+			return permutatedInput;
+		}
+  }
+
   // Front-end logic
   public static void main(String[] args) {
     Console console = System.console();
