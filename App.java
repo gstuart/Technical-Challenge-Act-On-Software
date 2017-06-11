4e6c02ae66c54c200527eb2a44d72d7377d63b87
Grace Stuart <gracerstuart@gmail.com>
2017-06-11 13:50:17 -0700

    add output to front-end logic

--- App.java
+++ App.java
@@ -1,2 +1,11 @@
+import java.io.Console;
+
 public class App {
+  // Front-end logic
+  public static void main(String[] args) {
+    Console console = System.console();
+    System.out.println("Please enter a string.");
+    String userInput = console.readLine();
+		System.out.println("\nString " + userInput + ":\nPermutations: " + permutationOfString(userInput));
+  }
 }
